c18c852398d188d876c7139d0a48e5764b274792
Raymo <shaneraymond58@gmail.com>
2024-04-26 03:10:24 +0000

    updated app.py

--- twitter-sportsodds/app.py
+++ twitter-sportsodds/app.py
@@ -33,34 +33,6 @@ url = f"https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/?apiKey={api_ke
 # Fetch API data
 api_data = get_api_data(url)
 
-# Example function to find middle bets
-def find_middle_bets(api_data):
-    middle_bets = []
-    for game in api_data:
-        spread_offers = []
-        for bookmaker in game['bookmakers']:
-            for market in bookmaker['markets']:
-                if market['key'] == 'spreads':
-                    spread_offers.extend(market['outcomes'])
-        
-        # Compare spreads to find potential middles
-        for offer1 in spread_offers:
-            for offer2 in spread_offers:
-                if offer1['point'] < 0 < offer2['point']:
-                    # Logic to determine if a middle bet exists between offer1 and offer2
-                    pass
-    return middle_bets
-
-# Example function to find arbitrage opportunities
-def find_arbitrage_opportunities(api_data):
-    arbitrage_opportunities = []
-    for game in api_data:
-        for market in game['bookmakers'][0]['markets']:
-            if market['key'] == 'h2h':
-                total_inverse_odds = sum([1/convert_american_to_decimal_odds(outcome['price']) for outcome in market['outcomes']])
-                if total_inverse_odds < 1:
-                    arbitrage_opportunities.append(game['id'])  # or some other identifier
-    return arbitrage_opportunities
 
 
 # Database connection parameters
@@ -105,15 +77,3 @@ finally:
     if conn:
         conn.close()
 
-# If API data is retrieved successfully, calculate bets
-if api_data:
-    # Perform calculations (you don't need to save the results to the database)
-    middle_bets = calculate_middle_bet(api_data)
-    arbitrage_bets = calculate_arbitrage_bet(api_data)
-    
-    # Output the results to the console or to a file/log, etc.
-    print("Middle Bets:", middle_bets)
-    print("Arbitrage Bets:", arbitrage_bets)
-
-else:
-    print("No data retrieved from the API.")
